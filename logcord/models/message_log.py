0509354de47e1e062c33cb02480a54f11cea2147
Den4200 <dennis@dennispham.me>
2021-01-13 22:36:50 -0500

    Create Message Log model.

---
+++ logcord/models/message_log.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+
+from pydantic import BaseModel, Field
+
+from logcord.models.message import Message
+
+
+class MessageLog(BaseModel):
+    """The model schema for a message log."""
+    id: str
+    created_at: datetime = Field(default_factory=datetime.utcnow)
+    messages: list[Message]
