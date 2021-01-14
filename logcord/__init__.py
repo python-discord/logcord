59668c2e1099da2386f2e9e90601facc3c60d366
Den4200 <dennis@dennispham.me>
2021-01-13 22:39:11 -0500

    Migrate from Mongo Engine to PyMongo.
    Also moved to a separate `db.py` file.

--- logcord/__init__.py
+++ logcord/__init__.py
@@ -1,12 +1,4 @@
-import os
-
 import hug
-from mongoengine import connect
-
-if not (MONGO_HOST := os.getenv('MONGO_HOST')):
-    raise RuntimeError('Missing `MONGO_HOST` environment variable..')
-
-connect(host=MONGO_HOST)
 
 
 @hug.get()
